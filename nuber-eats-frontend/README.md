2e8b7f31ab943496cbedd522113ddb1909facd1b
hyunjae <steveysung91@gmail.com>
2021-01-08 14:37:45 +0900

    #15.2 리액트 훅 폼 연습

--- nuber-eats-frontend/README.md
+++ nuber-eats-frontend/README.md
@@ -239,3 +239,58 @@ export const client = new ApolloClient({
 
       export default App;
       ```
+
+### React Hook Form
+
+- https://react-hook-form.com/api
+- 쉽게 Form 만들어서 검증까지 가능하게 도와주는 모듈
+
+```
+npm install react-hook-form
+```
+
+```
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export const LoggedOutRouter = () => {
+  const { register, watch, handleSubmit, errors } = useForm();
+  const onSubmit = () => {
+    console.log(watch());
+    // watch 쓰면 submit 된 object 반환해준다
+  };
+  const onInvalid = () => {
+    console.log("cant create account");
+  };
+  console.log(errors);
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            ref={register({
+              required: "This is required",
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            ref={register({ required: true })}
+            name="password"
+            type="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+```
--- nuber-eats-frontend/src/router/loggedOutRouter.tsx
+++ nuber-eats-frontend/src/router/loggedOutRouter.tsx
@@ -1,13 +1,58 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { isLoggedInVar } from "../apollo";
+
+interface IForm {
+  email: string;
+  password: string;
+}
+
 export const LoggedOutRouter = () => {
-  const onClick = () => {
-    isLoggedInVar(true); // 이렇게 만 해주면 된다
+  const { register, watch, handleSubmit, errors } = useForm<IForm>();
+  // watch 쓰면 submit 된 object 반환해준다
+  // errors 에는 어떤 부분이 틀렸는지 반환해준다
+  const onSubmit = () => {
+    console.log(watch());
   };
+  const onInvalid = () => {
+    console.log("cant create account");
+  };
+  console.log(errors);
   return (
     <div>
-      <h1>loggedout</h1>
-      <button onClick={onClick}>click to Login</button>
+      <h1>Logged Out</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            ref={register({
+              required: "This is required",
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+          {/* 에러 활용해서 경고 메시지 띄우기 부분 */}
+          {errors.email?.message && (
+            <span className="font-bold text-red-600">
+              {errors.email?.message}
+            </span>
+          )}
+          {errors.email?.type === "pattern" && (
+            <span className="font-bold text-red-600">Only gmail allowed</span>
+          )}
+        </div>
+        <div>
+          <input
+            ref={register({ required: true })}
+            name="password"
+            type="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
+      </form>
     </div>
   );
 };
